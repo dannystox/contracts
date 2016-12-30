cf82a9222233f654f15bc239aa82b0e7cb56001f
Boris <boris@crypti.me>
2016-12-30 04:44:11 +0300

    updated forecasting

--- truffle.js
+++ truffle.js
@@ -12,6 +12,6 @@ module.exports = {
   rpc: {
     host: "localhost",
     port: 8545,
-    from: "0xf06b9dc06dbb3172e52a9e47a01f708cb1719734"
+    from: "0x03e600f572078e25ab987a34d68c296c69e2ebc4"
   }
 };
