fe020bc3020ed5f022c56e839c7bfbd67e07b03d
Boris <boris@crypti.me>
2017-03-10 20:33:37 +0300

    updated example

--- truffle.example.js
+++ truffle.example.js
@@ -1,7 +1,14 @@
 module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      from: '0x03e600f572078e25ab987a34d68c296c69e2ebc4'
+    }
+  },
   rpc: {
     host: 'localhost',
-    port: 8545,
-    from: '0x03e600f572078e25ab987a34d68c296c69e2ebc4'
+    port: 8545
   }
 }
