a770dec1aac8aff5db6f0e139a7ff468e256d15f
Artem <artem@wings.ai>
2017-03-22 20:50:07 +0300

    integer types and widths changed, stages is enum, minor fixes

--- contracts/forecasts/CrowdPoll.sol
+++ contracts/forecasts/CrowdPoll.sol
@@ -3,18 +3,65 @@ pragma solidity ^0.4.2;
 import "../zeppelin/Ownable.sol";
 
 contract CrowdPoll is Ownable {
+  /* Stage of the process:
+    BeforePolling -> model parameters not yet assigned
+    PollingPeriod -> accounts place hashes of their data
+    RevealPeriod -> accounts reveal their votes
+    MakingStats -> reveal period finished, owner is obtaining statistics
+    PollingSucceeded -> stats available, polling succeeded, accounts can take their rewards
+    PollingFailed -> polling failed due to no votes, etc. Accounts can unlock their tokens
+  */
+  enum Stage {
+    BeforePolling,
+    PollingPeriod,
+    RevealPeriod,
+    MakingStats,
+    PollingSucceeded,
+    PollingFailed
+  }
+
   // Ctor - called by owner
   function CrowdPoll(
-    uint _proponentFee, int32[] _ratingLevels, int32 _maxRating,
+    uint _proponentFee,
+    uint _projectGoal, uint _projectMax, uint _bucketStep
+  ) {
+    // Q: Do we need argument checking here or let it be somewhere else?
+
+    stage = Stage.BeforePolling;
+
+    proponentFee = int128(_proponentFee);
+    projectGoal = int128(_projectGoal);
+    projectMax = int128(_projectMax);
+    bucketStep = int128(_bucketStep);
+    for (int128 i=0; i<=projectMax; i += bucketStep) {
+      buckets[i] = Bucket({ grades: 0, voters: 0, psi: 0 });
+    }
+
+    logMasks[0] = 0xFFFFFFFFFFFFFFFF;
+    logMasks[1] = 0xFFFFFFFF;
+    logMasks[2] = 0xFFFF;
+    logMasks[3] = 0xFF;
+    logMasks[4] = 0xF;
+    logMasks[5] = 3;
+    logMasks[6] = 1;
+  }
+
+  // Stage 0: The owner adds model parameters and
+  // after that accounts can start to vote
+  // if # rating levels < 8 then simply zeroes are passed
+  function add_model_parameters(
     int32 _spamThreshold,
     int32 _gamma1, int32 _theta1, int32 _gamma2, int32 _theta2,
     int32 _c, int32 _K,
-    uint _projectGoal, uint _projectMax, uint _bucketStep)
-  {
-    stage = 1;
-    proponentFee = _proponentFee;
-    ratingLevels = _ratingLevels;
-    maxRating = _maxRating;
+    int32 _maxRating,
+    int32 a1, int32 a2, int32 a3, int32 a4, int32 a5, int32 a6, int32 a7, int32 a8
+  ) onlyOwner() {
+    // Again, where do we need to check the sanity of all of this?
+
+    if (stage != Stage.BeforePolling) {
+      throw;
+    }
+
     spamThreshold = _spamThreshold;
     gamma1 = _gamma1;
     theta1 = _theta1;
@@ -22,27 +69,28 @@ contract CrowdPoll is Ownable {
     theta2 = _theta2;
     c = _c;
     K = _K;
-    projectGoal = _projectGoal;
-    projectMax = _projectMax;
-    bucketStep = _bucketStep;
-    for (uint i=0; i<=projectMax; i += bucketStep) {
-      buckets[i] = Bucket({ grades: 0, voters: 0, psi: 0 });
-    }
+    maxRating = _maxRating;
+    ratingLevels[0] = a1;
+    ratingLevels[1] = a2;
+    ratingLevels[2] = a3;
+    ratingLevels[3] = a4;
+    ratingLevels[4] = a5;
+    ratingLevels[5] = a6;
+    ratingLevels[6] = a7;
+    ratingLevels[7] = a8;
 
-    logMasks[0] = 0xFFFFFFFF00000000;
-    logMasks[1] = 0xFFFF0000;
-    logMasks[2] = 0xFF00;
-    logMasks[3] = 0xF0;
-    logMasks[4] = 0x0C;
-    logMasks[5] = 2;
+    stage = Stage.PollingPeriod;
   }
 
   // Q: sortof ACL, can anybody call those fns, need to be called only from accounts from system!
   // проверим есть ли у адреса токены или он оттуда
 
   // Stage 1: account posts hashed forecast
-  function post_forecast(uint value, uint bi, uint32 gi) {
-    if (stage != 1 || value > projectMax || (value % bucketStep) != 0 || bi == 0) {
+  function post_forecast(uint value, uint bi, uint8 gi) {
+    if (stage != Stage.PollingPeriod
+        || value > uint(projectMax)
+        || (value % uint(bucketStep)) != 0
+        || bi == 0) {
         throw;
     }
 
@@ -59,20 +107,23 @@ contract CrowdPoll is Ownable {
 
   // Stop voting, start reveal period, called by owner
   function start_reveal_period() onlyOwner() {
-    if (stage == 1) stage = 2;
+    if (stage == Stage.PollingPeriod) stage = Stage.RevealPeriod;
   }
 
   // Stage 2: accounts reveal their forecasts
-  function reveal_forecast(uint v, uint bi, uint32 gi) {
-    if (stage != 2) {
+  function reveal_forecast(uint _v, uint _bi, uint8 gi) {
+    if (stage != Stage.RevealPeriod) {
         throw;
     }
 
     address from = msg.sender;
 
-    if (hashedVotes[from] != sha256(from, v, bi, gi) || revealedVotes[from].balance != 0) {
+    if (hashedVotes[from] != sha256(from, _v, _bi, gi) || revealedVotes[from].balance > 0) {
       throw;
     }
+
+    int128 v = int128(_v);
+    int128 bi = int128(_bi);
 
     ++totalVotes;
     totalGrades += gi;
@@ -89,40 +140,43 @@ contract CrowdPoll is Ownable {
   // Q: return values of external calls
 
   // Stage 3: the owner stops reveal period and calculates stats
-  function make_stats(uint _tokensCollected, uint _totalTokensInSystem) onlyOwner() {
-    stage = 3;
-    totalTokensInSystem = _totalTokensInSystem;
-    collected = _tokensCollected;
+  function make_stats(uint _collected, uint _totalTokensInSystem) onlyOwner() {
+    stage = Stage.MakingStats;
+
+    totalTokensInSystem = int128(_totalTokensInSystem);
+    collected = int128(_collected);
     if (totalTokensInSystem < tokensVoted || totalGrades == 0) {
+      stage = Stage.PollingFailed;
       return;
     }
 
     spamFactor = make_ratio(buckets[0].grades, totalGrades);
     if (spamFactor > spamThreshold) {
       isSpam = true;
-      uint votersForSpam = buckets[0].voters;
-      if (votersForSpam) {
+      int64 votersForSpam = buckets[0].voters;
+      if (votersForSpam > 0) {
           rewardForSpamVote = proponentFee / votersForSpam;
           creatorRefund = proponentFee % votersForSpam;
           // so, do a refund right here or whatever
       } else {
           // TODO log illegal state
+          stage = Stage.PollingFailed;
           return;
       }
-      stage = 4;
+      stage = Stage.PollingSucceeded;
       return;
     }
 
-    uint q1Index = totalGrades / 4;
-    uint q3Index = totalGrades * 3 / 4;
-    uint spamGrades = buckets[0].grades;
-    uint mIndex = spamGrades + (totalGrades - spamGrades) / 2;
-    uint Q1 = NOT_FOUND;
-    uint Q3 = NOT_FOUND;
+    int q1Index = totalGrades / 4;
+    int q3Index = totalGrades * 3 / 4;
+    int spamGrades = buckets[0].grades;
+    int mIndex = spamGrades + (totalGrades - spamGrades) / 2;
+    int128 Q1 = NOT_FOUND;
+    int128 Q3 = NOT_FOUND;
     median = NOT_FOUND;
 
-    uint acc = 0;
-    for (uint i=0; i<=projectMax; i += bucketStep) {
+    int acc = 0;
+    for (int128 i=0; i<=projectMax; i += bucketStep) {
       Bucket bucket = buckets[i]; // Q: copied to memory ???
       acc += bucket.grades;
       if (Q1 == NOT_FOUND && acc >= q1Index) {
@@ -140,6 +194,7 @@ contract CrowdPoll is Ownable {
 
     if (Q3 == NOT_FOUND || median == NOT_FOUND || Q1 == NOT_FOUND) {
       // polling failed, no stats
+      stage = Stage.PollingFailed;
       return;
     }
 
@@ -153,20 +208,20 @@ contract CrowdPoll is Ownable {
     }
 
     int lsq = make_ratio(tokensVoted, totalTokensInSystem);
-    lsquare = (lsq*lsq) >> 10;
+    lsquare = int32((lsq*lsq) >> 10);
     valueOfPoll = (lsquare*q) >> 10;
 
-    for (uint j=0; j<=projectMax; j += bucketStep) {
+    for (int128 j=0; j<=projectMax; j += bucketStep) {
       buckets[j].psi = psi(j);
     }
 
-    stage = 4;
+    stage = Stage.PollingSucceeded;
   }
 
   function take_result(uint rating, uint g) returns(int deltaTokens, int deltaR, int deltaG) {
     // TODO what to do if stage == 3?
 
-    if (stage != 4) {
+    if (stage != Stage.PollingSucceeded) {
         // stats not yet collected, we are on wrong stage
         throw;
     }
@@ -203,12 +258,14 @@ contract CrowdPoll is Ownable {
     // куда все возвращать если этот вызов есть tx - начислять
   }
 
-  function make_ratio(uint num, uint denom) internal constant returns(int) {
+  function make_ratio(int128 num, int128 denom) internal constant returns(int32) {
     // 1024 is the base for fixed point ratios
-    return (num << 10) / denom;
+    return int32((num << 10) / denom);
   }
 
-  function psi(uint value) internal constant returns(int32) {
+  function psi(int128 value) internal constant returns(int32) {
+    // The default for function parameters (including return parameters) is memory, the default for local variables is storage
+
     // тоже спросить у макса что с неинициализированными переменными
     int gamma;
     int theta;
@@ -237,16 +294,16 @@ contract CrowdPoll is Ownable {
         Psi = -c;
     }
 
-    return Psi;
+    return int32(Psi);
   }
 
-  function log2(uint64 x) internal constant returns(uint64 y) {
+  function log2(int128 x) internal constant returns(int128 y) {
     y = 0;
-    uint64 c = 32;
-    for (int i=0; i<6; ++i) {
-      if ((x & logMasks[i]) != 0) {
+    int128 c = 64;
+    for (uint8 i=0; i<7; ++i) {
+      if (x > logMasks[i]) {
         y += c;
-        x >>= c; // ~~~~~~~~~~~~~ XXX
+        x >>= c;
       }
       c >>= 1;
     }
@@ -261,108 +318,110 @@ contract CrowdPoll is Ownable {
 
   }
 
-  uint constant NOT_FOUND = 1 << 128;
-  uint64 constant RESULT_TAKEN = 0xFFFFFFFFFFFFFFFF;
-  uint64[6] logMasks;
+// Project parameters
+  // Current stage of forecasting process
+  Stage public stage;
 
-  /* Stage of the process:
-    1 -> polling period
-    2 -> reveal period
-    3 -> reveal period finished, but stats are not ready (this stage is terminal if polling itself has failed)
-    4 -> stats available, polling succeeded
-  */
-  int stage;
-
-  // 'x' - proponent's fee that was taken
-  uint public proponentFee;
-
-  // rating levels a[i], (8 is max ??? to fit into 1 slot)
-  int32[] public ratingLevels; // mapping vs array ????
-
-  // 'R' - max forecast rating
-  int32 public maxRating;
-
-  // 's0' - spam threshold [0..1024]
-  int32 public spamThreshold;
-
-  // gamma, theta, c, K parameters for 'psi' function (*1024)
-  int32 public gamma1;
-  int32 public theta1;
-  int32 public gamma2;
-  int32 public theta2;
-  int32 public c;
-  int32 public K;
-
-  // goal of the project in tokens
-  uint public projectGoal;
-
-  // project's maximum in tokens
-  uint public projectMax;
-
-  // step of discrete bucket levels
-  uint bucketStep;
-
-  // value collected by the project
-  uint public collected;
-
-  // total tokens in system at the moment of collecting stats
-  uint public totalTokensInSystem;
-
-  // Spam factor of the poll
-  int32 public spamFactor;
-
-  // True if the poll considered spam
+  // True if the poll considered spam (zero votes # exceeds spamThreshold)
   bool public isSpam;
 
-  // reward to those who reported spam
-  uint rewardForSpamVote;
+  // 'x' - proponent's fee that was taken
+  int128 proponentFee;
 
-  // refund to creator to avoid losing tokens
-  uint creatorRefund;
+  // goal of the project in tokens
+  int128 projectGoal;
 
-  // median of votes distribution
-  int public median;
+  // project's maximum in tokens
+  int128 projectMax;
+
+  // step of discrete bucket levels
+  int128 bucketStep;
+
+// Model parameters
+  // rating levels a[i], (8 is max ??? to fit into 1 slot)
+  int32[8] ratingLevels; // mapping vs array ????
+
+  // 's0' - spam threshold [0..1024]
+  int32 spamThreshold;
+
+  // gamma, theta, c, K parameters for 'psi' and deltas functions (*1024)
+  int32 gamma1;
+  int32 theta1;
+  int32 gamma2;
+  int32 theta2;
+  int32 c;
+  int32 K;
+
+  // 'R' - max forecast rating
+  int32 maxRating;
+
+// Stats
+  // median of values (>0) distribution weighted by grades
+  int128 median;
+
+  // Spam factor s0 of the poll [0..1024]
+  int32 spamFactor;
 
   // Quartile coefficient of dispersion [0..1024]
-  uint32 public q;
+  int32 q;
 
   // squared 'l' in value of the poll formula [0..1024]
-  uint32 public lsquare;
+  int32 lsquare;
 
   // Value of the poll
-  uint32 public valueOfPoll;
+  int32 valueOfPoll;
 
   // total votes not weighted by g
-  uint32 totalVotes;
+  int64 totalVotes;
 
   // votes weighted by g
-  uint32 totalGrades;
+  int64 totalGrades;
 
   // total tokens voted
-  uint tokensVoted;
+  int128 tokensVoted;
+
+  // value collected by the project
+  int128 collected;
+
+  // total tokens in system at the moment of collecting stats
+  int128 totalTokensInSystem;
+
+  // reward to those who reported spam
+  int128 rewardForSpamVote;
+
+  // refund to creator to avoid losing tokens
+  int128 creatorRefund;
 
   struct Bucket {
     // Grade of the level, this is also histogram item
-    uint32 grades;
+    int64 grades;
 
     // voters distributed per level, w/o grade weighting
-    uint32 voters;
+    int64 voters;
 
-    // Psi (formula 5) values [0..1024]
+    // Psi (formula 5) values [-c..1024]
     int32 psi;
   }
 
+  // Vote revealed
+  struct Vote {
+    // Sum of the project
+    int128 value;
+
+    // Balance of account in tokens at the moment of the poll
+    int128 balance;
+  }
+
   // value => bucket, value = [0, bucketStep, ... projectGoal ... projectMax]
-  mapping(uint => Bucket) buckets;
+  mapping(int128 => Bucket) buckets;
 
   // hidden votes passed on stage 1
   mapping(address => bytes32) hashedVotes;
 
-  struct Vote {
-    uint value;
-    uint balance;
-  }
-
   // votes revealed on stage 2
   mapping(address => Vote) revealedVotes;
+
+  int128 constant NOT_FOUND = -1;
+  int128 constant RESULT_TAKEN = -1;
+  int128[7] logMasks;
 }
