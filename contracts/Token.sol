1b308105a31c5b005c21fbbda3b1ff5b3fac9bae
Boris <boris@crypti.me>
2017-03-30 14:12:53 +0300

    added events for premine, fixed possible two times allocation of same account, small refactoring, moving pre-verification of pre-miner account to modifier

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,9 +6,21 @@ import "./zeppelin/token/StandardToken.sol";
 /*
   Wings ERC20 Token.
   Added allocation for users who participiated in Wings Campagin.
+
+  Important!
+  We have to run pre-mine allocation first.
+  And only then rest of users.
+  Or it's not going to work due to whenAllocation logic.
 */
 contract Token is StandardToken, Ownable {
+  // Account allocation event
   event Allocation(address indexed account, uint amount);
+
+  /*
+    Premine events
+  */
+  event PreminerAdded(address indexed account, uint amount);
+  event PremineAllocationAdded(address indexed account, uint time);
   event PremineRelease(address indexed account, uint timestamp, uint amount);
 
   /*
@@ -36,11 +48,6 @@ contract Token is StandardToken, Ownable {
   uint public decimals = 18;
 
   /*
-    Is allocation completed?
-  */
-  bool public allocation;
-
-  /*
     How many accounts allocated?
   */
   uint public accountsToAllocate;
@@ -51,6 +58,28 @@ contract Token is StandardToken, Ownable {
   */
   modifier whenAllocation(bool value) {
     if ((accountsToAllocate > 0) == value) {
+      _;
+    } else {
+      throw;
+    }
+  }
+
+  /*
+    Check if user already allocated
+  */
+  modifier checkUserAllocation(address user) {
+    if (balances[user] == 0) {
+      _;
+    } else {
+      throw;
+    }
+  }
+
+  /*
+    Check if preminer already added
+  */
+  modifier checkPreminerAllocation(address preminer) {
+    if (preminers[preminer].account == address(0)) {
       _;
     } else {
       throw;
@@ -71,8 +100,10 @@ contract Token is StandardToken, Ownable {
   /*
     Allocate tokens for users.
     Only owner and only while allocation active.
+
+    Should check if user allocated already (no double allocations)
   */
-  function allocate(address user, uint balance) onlyOwner() whenAllocation(true) {
+  function allocate(address user, uint balance) onlyOwner() whenAllocation(true) checkUserAllocation(user) {
     balances[user] = balance;
 
     accountsToAllocate--;
@@ -112,11 +143,7 @@ contract Token is StandardToken, Ownable {
   /*
     Add pre-mine account
   */
-  function addPreminer(address preminer, uint initialBalance, uint monthlyPayment) onlyOwner() whenAllocation(true) {
-    if (preminers[preminer].account != address(0)) {
-      throw;
-    }
-
+  function addPreminer(address preminer, uint initialBalance, uint monthlyPayment) onlyOwner() whenAllocation(true) checkPreminerAllocation(preminer) {
     var premine = Preminer(
         preminer,
         monthlyPayment,
@@ -128,6 +155,7 @@ contract Token is StandardToken, Ownable {
     balances[preminer] = safeAdd(balances[preminer], initialBalance);
     preminers[preminer] = premine;
     accountsToAllocate--;
+    PreminerAdded(preminer, initialBalance);
   }
 
   /*
@@ -146,17 +174,32 @@ contract Token is StandardToken, Ownable {
       if (previousAllocation > _time) {
         throw;
       }
-
-      preminer.allocations[preminer.allocationsCount++] = _time;
-    } else {
-      preminer.allocations[preminer.allocationsCount++] = _time;
     }
+
+    preminer.allocations[preminer.allocationsCount++] = _time;
+    PremineAllocationAdded(_preminer, _time);
+  }
+
+  /*
+    Get preminer
+  */
+  function getPreminer(address _preminer) constant returns (uint, uint, uint) {
+    var preminer = preminers[_preminer];
+
+    return (preminer.monthlyPayment, preminer.latestAllocation, preminer.allocationsCount);
+  }
+
+  /*
+    Get preminer allocation time by index
+  */
+  function getPreminerAllocation(address _preminer, uint _index) constant returns (uint) {
+    return preminers[_preminer].allocations[_index];
   }
 
   /*
     Release premine when preminer asking
     Gas usage: 0x5786 or 22406 GAS.
-    Let's add limitation to 20 per cycle for cycle to be sure it could execute.
+    Maximum is 26 monthes of pre-mine in case of Wings. So should be enough to execute it.
   */
   function releasePremine() whenAllocation(false) {
     var preminer = preminers[msg.sender];
