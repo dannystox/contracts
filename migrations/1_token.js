05927012a78437acb5f16a7a2df65f3b8f0394dd
Boris <boris@crypti.me>
2017-05-02 05:01:14 +0300

    migrations for developing

--- migrations/1_token.js
+++ migrations/1_token.js
@@ -1,24 +1,15 @@
 const Promise = require('../node_modules/bluebird/js/release/bluebird.js')
 const Token = artifacts.require("../contracts/Token.sol")
-const WingsMultisigFactory = artifacts.require("../contracts/MultiSigWallet/WingsMultisigFactory.sol")
+const Wings = artifacts.require('../contracts/Wings.sol')
 
 module.exports = (deployer) => {
-  let multisig
-  const accounts = web3.eth.accounts.slice(8, 9)
+  const creator = web3.eth.accounts[0]
 
-  return deployer.deploy(WingsMultisigFactory).then(() => {
-    return WingsMultisigFactory.deployed()
-  }).then(_multisig => {
-    multisig = _multisig
-
-    return Promise.mapSeries(accounts, account => {
-      return multisig.addAddress(account)
-    })
-  }).then(() => {
-    return multisig.create(1)
-  }).then(() => {
-    return multisig.multisig.call()
-  }).then(multisigAddress => {
-    return deployer.deploy(Token, 1, multisigAddress)
+  return deployer.deploy(Token, 1, creator).then(() => {
+    return Token.deployed()
+  }).then(token => {
+    return token.allocate(creator, web3.toWei(100000000, 'ether')).then(() => token)
+  }).then(token => {
+    return deployer.deploy(Wings, token.address)
   })
 }
