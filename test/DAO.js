da97a35f75f1398ac59349f7ab495240062bc1e7
Boris <boris@crypti.me>
2017-05-14 19:45:43 +0300

    adopt DAO test to refactored code

--- test/DAO.js
+++ test/DAO.js
@@ -1,10 +1,6 @@
 const BigNumber = require('bignumber.js')
 const Promise = require('bluebird')
 
-const CrowdsaleFactory = artifacts.require('./crowdsales/CrowdsaleFactory.sol')
-const ForecastingFactory = artifacts.require('./forecasts/ForecastingFactory.sol')
-const MilestonesFactory = artifacts.require('./milestones/MilestonesFactory.sol')
-
 const DAO = artifacts.require("../contracts/DAO.sol")
 const Token = artifacts.require("../contracts/Token.sol")
 
@@ -24,8 +20,6 @@ contract('DAO', () => {
 
   let chance = new Chance()
   let dao, daoInfo
-
-  let token, milestones, forecasting
 
   before('Deploy Wings DAO', () => {
     daoInfo = {
@@ -53,51 +47,46 @@ contract('DAO', () => {
       daoInfo.milestones.push(milestone)
     }
 
-    return Token.new(web3.eth.accounts.length, {
-      from: creator
+    let currentTime, startTime, endTime
+
+    return time.blockchainTime(web3).then(_time => {
+      currentTime = _time
+
+      return Token.new(web3.eth.accounts.length, {
+        from: creator
+      })
     }).then(_token => {
       token = _token
 
-      /*
-      address _milestonesFactory,
-      address _forecastingFactory,
-      address _crowdsaleFactory
-      */
-      return Promise.join(
-        CrowdsaleFactory.new(),
-        ForecastingFactory.new(),
-        MilestonesFactory.new(),
-        (crowdsale, forecasting, milestones) => {
-            return DAO.new(
-                daoInfo.owner,
-                daoInfo.name,
-                daoInfo.symbol,
-                daoInfo.infoHash,
-                daoInfo.underCap,
-                daoInfo.reviewHours,
-                token.address,
-                milestones.address,
-                forecasting.address,
-                crowdsale.address,
-                {
-                  from: creator
-                })
-        }
-      )
+      return DAO.new(
+          creator,
+          creator,
+          crypto.createHash('sha256').update(daoInfo.name, 'utf8').digest().toString('hex'),
+          daoInfo.infoHash,
+          '0x0',
+          '0x0',
+          '0x0',
+          {
+            from: creator
+          })
     }).then(_dao => {
-      console.log('got dao')
       dao = _dao
       return dao.owner.call()
     }).then(owner => {
       assert.equal(owner, creator)
+
+      startTime = currentTime
+      endTime = startTime+3600
+      return dao.setTime(startTime, endTime, {
+        from: creator
+      })
     })
   })
 
-
-  it('Should allow to update DAO info before start', () => {
+  it('Should allow to update DAO info', () => {
     daoInfo.infoHash = '0x' + crypto.randomBytes(32).toString('hex')
 
-    return dao.update.sendTransaction(daoInfo.infoHash, {
+    return dao.update(daoInfo.infoHash, {
       from: creator
     }).then(() => {
       return dao.infoHash.call()
@@ -106,334 +95,32 @@ contract('DAO', () => {
     })
   })
 
-  it('Should allow to get milestones contract', () => {
-    return dao.milestones.call().then(address => {
-      return Milestones.at(address)
-    }).then(_milestones => {
-      milestones = _milestones
-    })
-  })
-
-  it('Should allow to add milestone', () => {
-    return Promise.each(daoInfo.milestones, (milestone) => {
-      return milestones.add.sendTransaction(milestone.amount, milestone.items, {
-        from: creator
-      })
+  it('Should move time after review period', () => {
+    return time.blockchainTime(web3).then(currentTime => {
+      return time.move(web3, currentTime+3601)
     }).then(() => {
-      return Promise.each(daoInfo.milestones, (milestone, index) => {
-        return milestones.get.call(index).then(milestoneRawData => {
-          const milestoneData = parser.parseMilestone(milestoneRawData)
-
-          assert.equal(milestone.amount.toString(10), milestoneData.amount.toString(10))
-          assert.equal(milestone.items, milestoneData.items)
-        })
-      })
-    }).then(() => {
-      return milestones.milestonesCount.call()
-    }).then(milestonesCount => {
-      assert.equal(milestonesCount.toString(10), daoInfo.milestones.length)
-    })
-  })
-
-  it('Should allow to update milestone', () => {
-    const i = chance.integer({min: 0, max: daoInfo.milestones.length - 1})
-
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    daoInfo.milestones[i] = newMilestone
-
-    return milestones.update.sendTransaction(i, newMilestone.amount, newMilestone.items, {
-      from: creator
-    }).then(() => {
-      return milestones.get(i)
-    }).then(milestoneRawData => {
-      const milestoneData = parser.parseMilestone(milestoneRawData)
-
-      assert.equal(daoInfo.milestones[i].amount.toString(10), milestoneData.amount.toString(10))
-      assert.equal(daoInfo.milestones[i].items, milestoneData.items)
-    })
-  })
-
-  it('Should doesnt allow to add more then 10 milestones', () => {
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    return milestones.add(newMilestone.amount, newMilestone.items).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-    })
-  })
-
-  it('Should allow to remove milestone', () => {
-    const i = 5
-
-    daoInfo.milestones = arrayHelper.remove(daoInfo.milestones, i)
-
-    return milestones.remove(i).then(() => {
-      return Promise.each(daoInfo.milestones, (milestone, index) => {
-        return milestones.get(index).then(milestoneRawData => {
-          const milestoneData = parser.parseMilestone(milestoneRawData)
-
-          assert.equal(milestone.amount.toString(10), milestoneData.amount.toString(10))
-          assert.equal(milestone.items, milestoneData.items)
-        })
-      })
-    }).then(() => {
-      return milestones.milestonesCount.call()
-    }).then(count => {
-      assert.equal(count.toString(10), daoInfo.milestones.length.toString(10))
-    })
-  })
-
-  it('Should allow to start DAO', () => {
-    /*
-      uint _forecastHours,
-      uint _crowdsaleHours,
-      address _multisig,
-      uint _initialPrice,
-      uint _rewardPercent
-    */
-    console.log(
-      daoInfo.forecastHours,
-      daoInfo.crowdsaleHours,
-      creator,
-      daoInfo.initialPrice,
-      daoInfo.rewardPercent
-    )
-    return dao.start(
-      daoInfo.forecastHours,
-      daoInfo.crowdsaleHours,
-      creator,
-      daoInfo.initialPrice,
-      daoInfo.rewardPercent,
-      {
-        from: creator
-    }).then(result => {
-      console.log(result)
-    })
-  })
-
-  it('Should allow to add new milestone after DAO started in review mode', () => {
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    daoInfo.milestones.push(newMilestone)
-
-    return milestones.add(newMilestone.amount, newMilestone.items)
-      .then(() => {
-        return Promise.each(daoInfo.milestones, (milestone, index) => {
-          return milestones.get(index).then(milestoneRawData => {
-            const milestoneData = parser.parseMilestone(milestoneRawData)
-
-            assert.equal(milestone.amount.toString(10), milestoneData.amount.toString(10))
-            assert.equal(milestone.items, milestoneData.items)
-          })
-        })
-      })
-  })
-
-  it('Should allow to update milestone after DAO started', () => {
-    const i = chance.integer({min: 0, max: daoInfo.milestones.length - 1})
-
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    daoInfo.milestones[i] = newMilestone
-
-    return milestones.update.sendTransaction(i, newMilestone.amount, newMilestone.items, {
-      from: creator
-    }).then(() => {
-      return milestones.get(i)
-    }).then(milestoneRawData => {
-      const milestoneData = parser.parseMilestone(milestoneRawData)
-
-      assert.equal(daoInfo.milestones[i].amount.toString(10), milestoneData.amount.toString(10))
-      assert.equal(daoInfo.milestones[i].items, milestoneData.items)
-    })
-  })
-
-  it('Should allow to remove milestones after DAO started', () => {
-    const i = 5
-
-    daoInfo.milestones = arrayHelper.remove(daoInfo.milestones, i)
-
-    return milestones.remove(i).then(() => {
-      return Promise.each(daoInfo.milestones, (milestone, index) => {
-        return milestones.get(index).then(milestoneRawData => {
-          const milestoneData = parser.parseMilestone(milestoneRawData)
-
-          assert.equal(milestone.amount.toString(10), milestoneData.amount.toString(10))
-          assert.equal(milestone.items, milestoneData.items)
-        })
-      })
-    }).then(() => {
-      return milestones.milestonesCount.call()
-    }).then(count => {
-      assert.equal(count.toString(10), daoInfo.milestones.length.toString(10))
-    })
-  })
-
-  it('Should doesnt allow  update to modificate, or update, or remove milestone', () => {
-    const i = 5
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    return milestones.remove(5).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-
-      return milestones.update(5, newMilestone.amount, newMilestone.items)
-    }).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-
-      return milestones.add(newMilestone.amount, newMilestone.items)
-    }).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-    })
-  })
-
-  it('Should allow to change project data while it is in review mode', () => {
-    daoInfo.infoHash = '0x' + crypto.randomBytes(32).toString('hex')
-
-    return dao.update.sendTransaction(daoInfo.infoHash, {
-      from: creator
-    }).then(() => {
-      return dao.infoHash.call()
-    }).then(infoHash => {
-      assert.equal(infoHash, daoInfo.infoHash)
-    })
-  })
-
-  it('Should get forecasting contract', () => {
-    return dao.forecasting.call().then(_forecasting => {
-      return Forecasting.at(_forecasting)
-    }).then(_forecasting => {
-      forecasting = _forecasting
-    })
-  })
-
-  it('Shouldnt allow to add forecast', () => {
-    return forecasting.add.sendTransaction(web3.toWei(chance.integer({min: 1, max: 1000 }), 'ether'), '0x' + crypto.randomBytes(32).toString('hex'))
-      .catch(err => {
-        assert.equal(errors.isJump(err.message), true)
-      })
-  })
-
-  it('Should move time to forecast period', () => {
-    const secondsToMove = (daoInfo.reviewHours * 3600)
-
-    return time.move(web3, secondsToMove).then(() => {
       return miner.mine(web3)
     }).then(() => {
-      return time.blockchainTime(web3).then(blockchainTime => {
-        return Promise.join(
-          forecasting.startTimestamp.call(),
-          forecasting.endTimestamp.call(),
-          (start, end) => {
-            assert.equal(blockchainTime > start.toNumber() && blockchainTime < end.toNumber(), true)
-          })
-      })
+      return Promise.join(
+        time.blockchainTime(web3),
+        dao.endTimestamp.call(),
+        (currentTime, endTimestamp) => {
+          assert.equal(endTimestamp.lt(currentTime), true)
+        }
+      )
     })
   })
 
-  it('Should doesnt allow to update DAO', () => {
+  it('Shouldnt possible to change info hash', () => {
     const infoHash = '0x' + crypto.randomBytes(32).toString('hex')
 
-    return dao.update.sendTransaction(infoHash, {
+    return dao.update(infoHash, {
       from: creator
-    }).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-    })
-  })
-
-  it('Should doesnt allow to update milestones', () => {
-    const i = chance.integer({min: 0, max: daoInfo.milestones.length - 1})
-
-    const newMilestone = {
-      amount: web3.toWei(chance.integer({min: 10, max: 1000}), 'ether'),
-      items: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    return milestones.update.sendTransaction(i, newMilestone.amount, newMilestone.items, {
-      from: creator
-    }).catch(err => {
-      assert.equal(errors.isJump(err.message), true)
-    })
-  })
-
-  it('Should add forecast', () => {
-    const forecast = {
-      address: web3.eth.accounts[1],
-      amount: web3.toWei(chance.integer({min: 1, max: 1000 }), 'ether'),
-      message: '0x' + crypto.randomBytes(32).toString('hex')
-    }
-
-    daoInfo.forecasts.push(forecast)
-
-    return forecasting.add.sendTransaction(forecast.amount, forecast.message, {
-      from: forecast.address
-    })
-  })
-
-  it('Should contains one forecast', () => {
-    return forecasting.forecastsCount.call().then(count => {
-      assert.equal(count.toString('10'), 1)
-    })
-  })
-
-  it('Should return one forecast object', () => {
-    return forecasting.get.call(0).then(forecastRawData => {
-      const forecast = parser.parseForecast(forecastRawData)
-
-      assert.equal(daoInfo.forecasts[0].address, forecast.creator)
-      assert.equal(daoInfo.forecasts[0].amount.toString('10'), forecast.amount.toString('10'))
-      assert.equal(daoInfo.forecasts[0].message, forecast.message)
-    })
-  })
-
-  it('Should return user forecast', () => {
-    return forecasting.getByUser.call(daoInfo.forecasts[0].address).then((forecastRawData) => {
-      const forecast = parser.parseUserForecast(forecastRawData)
-
-      assert.equal(daoInfo.forecasts[0].amount.toString('10'), forecast.amount.toString('10'))
-      assert.equal(daoInfo.forecasts[0].message, forecast.message)
-    })
-  })
-
-  it('Should complete forecast period', () => {
-    const secondsToMove = (daoInfo.forecastHours * 3600)
-
-    return time.move(web3, secondsToMove).then(() => {
-      return miner.mine(web3)
     }).then(() => {
-      return time.blockchainTime(web3).then(blockchainTime => {
-        return Promise.join(
-          forecasting.startTimestamp.call(),
-          forecasting.endTimestamp.call(),
-          (start, end) => {
-            assert.equal(blockchainTime > end.toNumber(), true)
-          })
-      })
+      throw new Error('Should return JUMP error')
+    }).catch(err => {
+      assert.equal(errors.isJump(err.message), true)
     })
-  })
-
-  it('Should does not allow to add new forecast', () => {
-    return forecasting.add.sendTransaction(
-      web3.toWei(chance.integer({min: 1, max: 1000 }), 'ether'),
-      '0x' + crypto.randomBytes(32).toString('hex'), {
-        from: web3.eth.accounts[3]
-      }).catch(err => {
-        assert.equal(errors.isJump(err.message), true)
-      })
   })
 
 })
